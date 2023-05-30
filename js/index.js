5d5a470069bbd611d5022e705f6bcf6dc3b02088
Web-Dev2022-JP <johnpaulquintana.iskolar@gmail.com>
2023-05-29 18:33:22 -0800

    updates bugs solved on home and shop

--- js/index.js
+++ js/index.js
@@ -9,90 +9,101 @@ let navbar = document.querySelector('.navbar')
 // new way of toggle and remove
 const elementsToRemoveActiveClass = [searchForm, cart, registerForm, navbar];
 
-document.querySelector('#search-btn').onclick = () => {
-    console.log('clicked')
-    searchForm.classList.toggle('active')
-    cart.classList.remove('active')
-    loginForm.classList.remove('active')
-    registerForm.classList.remove('active')
-    navbar.classList.remove('active')
-}
-// old version
-// let cart = document.querySelector('.shopping-cart')
 
-document.querySelector('#cart-btn').onclick = () => {
-    console.log('clicked')
-    cart.classList.toggle('active')
-    searchForm.classList.remove('active')
-    // cart.classList.remove('active')
-    loginForm.classList.remove('active')
-    registerForm.classList.remove('active')
-    navbar.classList.remove('active')
-}
-
-// old version
-// let loginForm = document.querySelector('.login-form')
-
-document.querySelector('#login-btn').onclick = () => {
-    console.log('clicked')
-    loginForm.classList.toggle('active')
-    searchForm.classList.remove('active')
-    cart.classList.remove('active')
-    // loginForm.classList.remove('active')
-    registerForm.classList.remove('active')
-    navbar.classList.remove('active')
-}
-
-// going back to login
-document.querySelector('#back-to-login').onclick = () => {
-    console.log('clicked');
-    loginForm.classList.toggle('active');
-    // this one line iteration of removing active class
-    elementsToRemoveActiveClass.forEach(element => element.classList.remove('active'));
-};
-
-// old version
-// let registerForm = document.querySelector('.registration-form')
-
-document.querySelector('#registration-btn').onclick = () => {
-    console.log('clicked')
-    loginForm.classList.remove('active')
-    searchForm.classList.remove('active')
-    cart.classList.remove('active')
-    registerForm.classList.toggle('active')
-    navbar.classList.remove('active')
-}
-
-// old version
-// let navbar = document.querySelector('.navbar')
-
-document.querySelector('#menu-btn').onclick = () => {
-    console.log('clicked')
-    navbar.classList.toggle('active')
-    searchForm.classList.remove('active')
-    cart.classList.remove('active')
-    loginForm.classList.remove('active')
-    registerForm.classList.remove('active')
-    // navbar.classList.remove('active')
-}
-
-window.onscroll = () => {
-    searchForm.classList.remove('active')
-    cart.classList.remove('active')
-    loginForm.classList.remove('active')
-    navbar.classList.remove('active')
-    registerForm.classList.remove('active')
-}
-
-let slides = document.querySelectorAll('.home .slides-container .slide')
-let index = 0
-function next(){
-    slides[index].classList.remove('active')
-    index = (index + 1) % slides.length
-    slides[index].classList.add('active')
-}
-function prev(){
-    slides[index].classList.remove('active')
-    index = (index - 1 + slides.length) % slides.length
-    slides[index].classList.add('active')
-}
+    document.querySelector('#search-btn').onclick = () => {
+        console.log('clicked')
+        searchForm.classList.toggle('active')
+        cart.classList.remove('active')
+        loginForm.classList.remove('active')
+        registerForm.classList.remove('active')
+        navbar.classList.remove('active')
+    }
+    // old version
+    // let cart = document.querySelector('.shopping-cart')
+    
+    document.querySelector('#cart-btn').onclick = () => {
+        console.log('clicked')
+        cart.classList.toggle('active')
+        searchForm.classList.remove('active')
+        // cart.classList.remove('active')
+        loginForm.classList.remove('active')
+        registerForm.classList.remove('active')
+        navbar.classList.remove('active')
+    }
+    
+    // old version
+    // let loginForm = document.querySelector('.login-form')
+    
+    document.querySelector('#login-btn').onclick = () => {
+        console.log('clicked')
+        loginForm.classList.toggle('active')
+        searchForm.classList.remove('active')
+        cart.classList.remove('active')
+        // loginForm.classList.remove('active')
+        registerForm.classList.remove('active')
+        navbar.classList.remove('active')
+    }
+    // 
+    
+    
+    document.querySelector('#back-to-login').onclick = () => {
+        console.log('clicked');
+        loginForm.classList.toggle('active');
+        elementsToRemoveActiveClass.forEach(element => element.classList.remove('active'));
+    };
+    
+    // old version
+    // let registerForm = document.querySelector('.registration-form')
+    
+    document.querySelector('#registration-btn').onclick = () => {
+        console.log('clicked')
+        loginForm.classList.remove('active')
+        searchForm.classList.remove('active')
+        cart.classList.remove('active')
+        registerForm.classList.toggle('active')
+        navbar.classList.remove('active')
+    }
+    
+    // old version
+    // let navbar = document.querySelector('.navbar')
+    
+    document.querySelector('#menu-btn').onclick = () => {
+        console.log('clicked')
+        navbar.classList.toggle('active')
+        searchForm.classList.remove('active')
+        cart.classList.remove('active')
+        loginForm.classList.remove('active')
+        registerForm.classList.remove('active')
+        // navbar.classList.remove('active')
+    }
+    
+    window.onscroll = () => {
+        searchForm.classList.remove('active')
+        cart.classList.remove('active')
+        loginForm.classList.remove('active')
+        navbar.classList.remove('active')
+        registerForm.classList.remove('active')
+    }
+    
+    let slides = document.querySelectorAll('.home .slides-container .slide')
+    let index = 0
+    function next(){
+        clearInterval(autoAdvanceTimer); // Clear the auto advance timer
+        slides[index].classList.remove('active')
+        index = (index + 1) % slides.length
+        slides[index].classList.add('active')
+    }
+    function prev(){
+        clearInterval(autoAdvanceTimer); // Clear the auto advance timer
+        slides[index].classList.remove('active')
+        index = (index - 1 + slides.length) % slides.length
+        slides[index].classList.add('active')
+    }
+    function autoAdvance() {
+        slides[index].classList.remove('active');
+        index = (index + 1) % slides.length;
+        slides[index].classList.add('active');
+    }
+    
+    // Start auto advancing every 5 seconds
+    autoAdvanceTimer = setInterval(autoAdvance, 5000);
